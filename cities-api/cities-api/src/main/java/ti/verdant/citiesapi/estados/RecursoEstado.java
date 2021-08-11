d2cddfa061e5acba5c2cc4067d7e81acb85eb65f
Fatinha de Sousa <fatinha.sg@hotmail.com>
2021-08-11 13:27:05 -0300

    Criando o metodo de buscar um estado.

--- cities-api/cities-api/src/main/java/ti/verdant/citiesapi/estados/RecursoEstado.java
+++ cities-api/cities-api/src/main/java/ti/verdant/citiesapi/estados/RecursoEstado.java
@@ -1,12 +1,15 @@
 package ti.verdant.citiesapi.estados;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ti.verdant.citiesapi.repositorios.RepositorioEstado;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/estados")
@@ -21,5 +24,17 @@ public class RecursoEstado {
     @GetMapping
     public List<Estado> listarEstados(){
         return repositorioEstado.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity buscarEstado(@PathVariable long id){
+        Optional<Estado> optional = repositorioEstado.findById(id);
+
+        if(optional.isPresent()){
+            return  ResponseEntity.ok().body(optional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
